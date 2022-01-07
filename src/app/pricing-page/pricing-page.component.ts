de3f3c6e6d69a275908df2769825154160aab33e
sassi <attila.sassi@gmail.com>
2022-01-07 01:00:44 +0100

    kész feladat

--- src/app/pricing-page/pricing-page.component.ts
+++ src/app/pricing-page/pricing-page.component.ts
@@ -14,11 +14,32 @@ interface IPriceItem {
 })
 export class PricingPageComponent implements OnInit {
 
-  priceTable: IPriceItem[] = [];
+  priceTable: IPriceItem[] = [
+    {
+      name: 'Free', 
+      price: 0, options: 
+      ['10 tanuló', '2GB tárhely', 'Email támogatás', 'Dokumentáció'], 
+      btnText: 'Ingyenes regisztráció'
+    },
+    {
+      name: 'Pro', 
+      price: 15, options: 
+      ['20 tanuló', '10 GB tárhely', 'Kiemelt email támogatás', 'Dokumentáció'], 
+      btnText: 'Kezdő lépések'
+    },
+    {
+      name: 'Enterprise', 
+      price: 29, options: 
+      ['100 tanuló', '100 GB tárhely', 'Telefonos és email támogatás', 'Dokumentáció'], 
+      btnText: 'Kontakt'
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    Array.from(document.querySelectorAll('.btn.btn-lg.btn-block.btn-outline-primary'))
+        .forEach((e, i) => i ? (e.classList.add('btn-primary'), e.classList.remove('btn-outline-primary')) : 0);
   }
 
 }
